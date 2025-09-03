ab807d2458c95937a49e5e6152febaef13539ec4
Alex Gudev <alexgud2012@gmail.com>
2025-09-03 13:28:09 +0100

    add .editorconfig to fix this silly 2-space indent xD

--- app/BoardGen.hs
+++ app/BoardGen.hs
@@ -19,48 +19,48 @@ type CellUpdater g b = (b -> [b] -> State g b)
 type BoardSize = (Int, Int)
 
 makePureBoards ::
-  forall g b.
-  (RandomGen g) =>
-  BoardSize ->
-  g ->
-  b ->
-  CellUpdater g b ->
-  [Array (Int, Int) b]
+    forall g b.
+    (RandomGen g) =>
+    BoardSize ->
+    g ->
+    b ->
+    CellUpdater g b ->
+    [Array (Int, Int) b]
 makePureBoards size g defBlock weigh = runST $ do
-  gRef <- strictToLazyST $ newSTRef g
-  board <- initBoard defBlock size
-  let
-    go = do
-      a <- freeze board
-      nextBoard gRef board weigh
-      rest <- unsafeInterleaveST go
-      pure (a : rest)
-  go
+    gRef <- strictToLazyST $ newSTRef g
+    board <- initBoard defBlock size
+    let
+        go = do
+            a <- freeze board
+            nextBoard gRef board weigh
+            rest <- unsafeInterleaveST go
+            pure (a : rest)
+    go
 
 nextBoard :: (RandomGen g) => STRef s g -> Board s b -> CellUpdater g b -> ST s ()
 nextBoard gRef board weigh = do
-  assocs <- getAssocs board
-  forM_ assocs $ updateCell . fst
- where
-  updateCell i = do
-    nbs <- getNeighbours i board
-    strictToLazyST $ do
-      val <- readArray board i
-      g <- readSTRef gRef
-      let (res, g') = runState (weigh val nbs) g
-      writeSTRef gRef g'
-      writeArray board i res
+    assocs <- getAssocs board
+    forM_ assocs $ updateCell . fst
+  where
+    updateCell i = do
+        nbs <- getNeighbours i board
+        strictToLazyST $ do
+            val <- readArray board i
+            g <- readSTRef gRef
+            let (res, g') = runState (weigh val nbs) g
+            writeSTRef gRef g'
+            writeArray board i res
 
 getNeighbours :: (Int, Int) -> Board s b -> ST s [b]
 getNeighbours (i, j) board = do
-  bounds <- getBounds board
-  let inBounds = inRange bounds
-  sequence $ do
-    i' <- [i - 1 .. i + 1]
-    j' <- [j - 1 .. j + 1]
-    guard $ (i', j') /= (i, j)
-    guard $ inBounds (i', j')
-    pure $ readArray board (i', j')
+    bounds <- getBounds board
+    let inBounds = inRange bounds
+    sequence $ do
+        i' <- [i - 1 .. i + 1]
+        j' <- [j - 1 .. j + 1]
+        guard $ (i', j') /= (i, j)
+        guard $ inBounds (i', j')
+        pure $ readArray board (i', j')
 
 initBoard :: a -> BoardSize -> ST s (Board s a)
 initBoard _ (r, c) | r < 1 || c < 1 = error "initBoard expects positive rows and cols."
